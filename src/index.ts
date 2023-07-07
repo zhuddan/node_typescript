9307a006946452056af5679b7de51182a816b176
zd <yobaci@163.com>
2023-07-07 15:48:36 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import './transformRuoyiVue';
+import { vueFile } from './transformRuoyiVue';
 
 console.log('\x1B[36m%s\x1B[0m', 'Welcome');
+
+vueFile.fixBase().write();
--- src/transformRuoyiVue.ts
+++ src/transformRuoyiVue.ts
@@ -4,7 +4,7 @@ import path from 'path';
 const inputFilePath = path.resolve(__dirname, 'input', 'index.vue');
 const outFilePath = path.resolve(__dirname, 'output', 'index.vue');
 
-const result = fs.readFileSync(inputFilePath).toString();
+// const result = fs.readFileSync(inputFilePath).toString();
 
 const prefix = 'dataSource';
 
@@ -35,71 +35,17 @@ function insertCharacter(str: string, char: string, index: number): string {
   }
 }
 
-let outputResult = result
-  .replaceAll('queryParams', `${prefix}QueryParams`)
-  .replaceAll('queryForm', `${prefix}QueryFormRef`)
-  .replaceAll('handleQuery', `handleQuery${toUpperCamelCase(prefix)}`)
-  .replaceAll('resetQuery', `resetQuery${toUpperCamelCase(prefix)}`)
-  .replaceAll('handleAdd', `handleAdd${toUpperCamelCase(prefix)}`)
-  .replaceAll('handleUpdate', `handleUpdate${toUpperCamelCase(prefix)}`)
-  .replaceAll('handleDelete', `handleDelete${toUpperCamelCase(prefix)}`)
-  .replaceAll('handleExport', `handleExport${toUpperCamelCase(prefix)}`)
-  .replaceAll('submitForm', `submitForm${toUpperCamelCase(prefix)}`)
+function updateStyle(input: string) {
+  const res = input
+    .replaceAll('// 非单个禁用', '')
+    .replaceAll('single: true,', '')
+    .replaceAll('// 非多个禁用', '')
+    .replaceAll('multiple: true,', '')
+    .replaceAll('this.single = selection.length !== 1;', '')
+    .replaceAll('this.multiple = !selection.length;', '')
+    .replaceAll('response', 'res');
 
-  .replaceAll('cancel', `handle${toUpperCamelCase(prefix)}Cancel`)
-  .replaceAll('reset()', `handle${toUpperCamelCase(prefix)}Reset()`)
-
-  .replaceAll('getList', `get${toUpperCamelCase(prefix)}List`)
-  .replaceAll('handleSelectionChange,', `handleSelectionChange${toUpperCamelCase(prefix)}`)
-
-  .replaceAll('btnLoading', `${prefix}BtnLoading`)
-  .replaceAll('v-loading="loading"', `v-loading="${prefix}Loading"`)
-  .replaceAll('loading: true,', `${prefix}Loading: true,`)
-  .replaceAll('this.loading', `this.${prefix}Loading`)
-  //
-  .replaceAll(':total', ':TOTAL')
-  .replaceAll('total', `${prefix}Total`)
-  .replaceAll(':TOTAL', ':total')
-  //
-  .replaceAll(':title', ':TITLE')
-  .replaceAll('title', `${prefix}Title`)
-  .replaceAll(':TITLE', ':title')
-
-  .replaceAll('open', `${prefix}Open`)
-  .replaceAll('ids', `${prefix}Ids`)
-  .replaceAll('showSearch', `${prefix}ShowSearch`)
-
-  .replaceAll('// 非单个禁用', '')
-  .replaceAll('single: true,', '')
-
-  .replaceAll('// 非多个禁用', '')
-  .replaceAll('multiple: true,', '')
-  .replaceAll('this.single = selection.length !== 1;', '')
-  .replaceAll('this.multiple = !selection.length;', '')
-
-  .replaceAll('ref="form"', `ref="${prefix}FormRef"`)
-  .replaceAll('this.resetForm("form")', `this.resetForm("${prefix}FormRef")`)
-
-  .replaceAll(':model="form"', `:model="${prefix}Form"`)
-  .replaceAll('this.form', `this.${prefix}Form`)
-  .replaceAll('form:', `${prefix}Form:`)
-  .replaceAll('rules:', `${prefix}Rules:`)
-  .replaceAll('response', 'res')
-
-// style
-
-  .replaceAll(':inline="true"', 'inline')
-
-  .replace(/^\s*[\r\n]/gm, '')
-
-  .replaceAll('import {', '\n\nimport {')
-  .replaceAll('export default', '\n\nexport default')
-
-  ;
-
-const created_index = outputResult.indexOf('created()');
-
-outputResult = insertCharacter(outputResult, `
+  return insertCharacter(res, `
   computed: {
     // 非单个禁用
     single() {
@@ -111,8 +57,83 @@ outputResult = insertCharacter(outputResult, `
     },
   },
   
-`, created_index);
+`, res.indexOf('created()'));
+}
 
-fs.writeFileSync(outFilePath, outputResult);
+function updateRuoyiStyle(input: string) {
+  return input
+    .replaceAll('queryParams', `${prefix}QueryParams`)
+    .replaceAll('queryForm', `${prefix}QueryFormRef`)
+    .replaceAll('handleQuery', `handleQuery${toUpperCamelCase(prefix)}`)
+    .replaceAll('resetQuery', `resetQuery${toUpperCamelCase(prefix)}`)
+    .replaceAll('handleAdd', `handleAdd${toUpperCamelCase(prefix)}`)
+    .replaceAll('handleUpdate', `handleUpdate${toUpperCamelCase(prefix)}`)
+    .replaceAll('handleDelete', `handleDelete${toUpperCamelCase(prefix)}`)
+    .replaceAll('handleExport', `handleExport${toUpperCamelCase(prefix)}`)
+    .replaceAll('submitForm', `submitForm${toUpperCamelCase(prefix)}`)
 
-export {};
+    .replaceAll('cancel', `handle${toUpperCamelCase(prefix)}Cancel`)
+    .replaceAll('reset()', `handle${toUpperCamelCase(prefix)}Reset()`)
+
+    .replaceAll('getList', `get${toUpperCamelCase(prefix)}List`)
+    .replaceAll('handleSelectionChange,', `handleSelectionChange${toUpperCamelCase(prefix)}`)
+
+    .replaceAll('btnLoading', `${prefix}BtnLoading`)
+    .replaceAll('v-loading="loading"', `v-loading="${prefix}Loading"`)
+    .replaceAll('loading: true,', `${prefix}Loading: true,`)
+    .replaceAll('this.loading', `this.${prefix}Loading`)
+  //
+    .replaceAll(':total', ':TOTAL')
+    .replaceAll('total', `${prefix}Total`)
+    .replaceAll(':TOTAL', ':total')
+  //
+    .replaceAll(':title', ':TITLE')
+    .replaceAll('title', `${prefix}Title`)
+    .replaceAll(':TITLE', ':title')
+
+    .replaceAll('open', `${prefix}Open`)
+    .replaceAll('ids', `${prefix}Ids`)
+    .replaceAll('showSearch', `${prefix}ShowSearch`)
+
+    .replaceAll('ref="form"', `ref="${prefix}FormRef"`)
+    .replaceAll('this.resetForm("form")', `this.resetForm("${prefix}FormRef")`)
+
+    .replaceAll(':model="form"', `:model="${prefix}Form"`)
+    .replaceAll('this.form', `this.${prefix}Form`)
+    .replaceAll('form:', `${prefix}Form:`)
+    .replaceAll('rules:', `${prefix}Rules:`);
+}
+
+function done(str: string) {
+  return str
+    .replace(/^\s*[\r\n]/gm, '')
+    .replaceAll('import {', '\n\nimport {')
+    .replaceAll('export default', '\n\nexport default');
+}
+
+class VueFile {
+  content: string;
+  constructor(path: string) {
+    this.content = fs.readFileSync(path).toString();
+  }
+
+  fixBase() {
+    this.content = updateStyle(this.content);
+    return this;
+  }
+
+  updateRuoyiStyle() {
+    this.content = updateRuoyiStyle(this.content);
+    return this;
+  }
+
+  private get result() {
+    return done(this.content);
+  }
+
+  write() {
+    fs.writeFileSync(outFilePath, this.result, { flag: 'w' });
+  }
+}
+
+export const vueFile = new VueFile(inputFilePath);
