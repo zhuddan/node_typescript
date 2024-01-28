60dbd261c422bd828aca4e1e72810af7b597cd08
zd <476182045@qq.com>
2024-01-28 18:21:47 +0800

    d

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,37 @@
 import { SerialPort } from 'serialport';
 
-async function listSerialPorts() {
-  try {
-    SerialPort.list()
-      .then((ports) => {
-        console.log('ports', ports);
-
-        if (ports.length === 0) {
-          // document.getElementById('error')!.textContent = 'No ports discovered';
-        }
-
-        // tableHTML = tableify(ports)
-        // document.getElementById('ports').innerHTML = tableHTML
-      })
-      .catch((err) => {
-        if (err) {
-          // document.getElementById('error')!.textContent = err.message;
-          return;
-        }
-        else {
-          // document.getElementById('error')!.textContent = '';
-        }
+SerialPort.list()
+  .then((ports) => {
+    console.log(ports.map(e => e.path));
+    if (ports.length === 0) {
+    }
+    else {
+      const port = new SerialPort({
+        path: 'COM5',
+        baudRate: 2400,
       });
-  }
-  catch (error) {
-    console.log('After debugger', error);
-    // document.getElementById('error')!.textContent = error.message
-  }
-}
 
-function listPorts() {
-  listSerialPorts();
-  setTimeout(listPorts, 2000);
-}
-// // Set a timeout that will check for new serialPorts every 2 seconds.
-// // This timeout reschedules itself.
-setTimeout(listPorts, 2000);
+      port.on('open', () => {
+        console.log('[open]');
+        const hexData = 'FE FE FE FE 68 48 86 00 00 16 09 68 11 04 33 36 34 35 A4 16'.split(' ').join('');
+        const bufferData = Buffer.from(hexData, 'hex');
+        port.write(bufferData, (err) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log('send msg', bufferData);
+        });
+      });
+
+      port.on('data', (data) => {
+        console.log(data);
+      });
+    }
+
+  // tableHTML = tableify(ports)
+  // document.getElementById('ports').innerHTML = tableHTML
+  })
+  .catch(() => {
+
+  });
